507049e63aaeeb545d7b7f4c0caaf3d8ec344bf9
Jiawen Lu <luckykevvv@gmail.com>
2024-08-16 01:31:20 +1000

    finished all tts

--- gpt_api.py
+++ gpt_api.py
@@ -11,7 +11,7 @@ openai.api_key = get_api_key()
 
 class ChatGPT():
     def __init__(self) -> None:
-        self.message_init = [{"role": "system", "content": "You are a vtuber called open-source sama."}]
+        self.message_init = [{"role": "system", "content": "You are a vtuber called open-source sama, remember that vtuber dont ask any question, don't end with question marks"}]
         self.messages = self.message_init
 
     def ask_gpt(self):
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import package.live2d.v3 as live2d
 import sys
+import os
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import QTimer
-
+from ffmpeg import audio
 from show_model import ShowModel
 from text_window import TextWindow
 from show_text import ShowText
 from gpt_api import ChatGPT
+from gtts import gTTS
+import pyttsx3
+from audiostretchy.stretch import stretch_audio
+import pygame
 
 live2d.init()
-
+pygame.init()
+pygame.mixer.init()
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -24,6 +30,7 @@ class MainWindow(QMainWindow):
         self.show_text.updateText("Hello, I'm Open-Source Sama")
 
         self.chat = ChatGPT()
+        self.engine = pyttsx3.init()
 
         # Set up a timer to check the text_window.show_text attribute
         self.timer = QTimer(self)
@@ -33,7 +40,14 @@ class MainWindow(QMainWindow):
     def check_text_window(self):
         show=self.text_window.get_text()
         if show:
-            self.show_text.updateText(self.chat.asking(show))
+            result=self.chat.asking(show)
+            tts = gTTS(text=result, lang='en',slow=False)
+            tts.save('test.wav')
+            '''stretch_audio("test.wav", "test.wav", ratio=1.1)'''
+            pygame.mixer.Sound('test.wav').play()
+            '''self.engine.say(result)
+            self.engine.runAndWait()'''
+            self.show_text.updateText(result)
 
 app = QApplication(sys.argv)
 main_window = MainWindow()
